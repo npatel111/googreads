64f08268b380eb7d567d35b36e27570913fb4ff4
Niti Nrw <niti.patel@flatironschool.com>
2017-01-06 16:48:34 -0500

    Full CRUD actions for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,45 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      byebug
+      render :new
+    end
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  def index
+    @reviews = Review.all
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :book_id)
+  end
 end
