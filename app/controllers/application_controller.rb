b2ed80b1cfced67257d2feea3899cfe1692490f6
Niti Nrw <niti.patel@flatironschool.com>
2018-03-28 21:55:56 -0400

    Fix login bug so you get a welcome screen if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+    # byebug
     # User.find(session[:user_id]) || nil
-    User.find(session[:user_id])
+    session[:user_id] ? User.find(session[:user_id]) : nil
   end
 
   def logged_in?
+    byebug
     current_user.id != nil
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
 
-  before_action :require_login
+  # before_action :require_login
 
   def hello
+    # byebug
     @current_user  = current_user
+    # byebug
   end
 
 end
