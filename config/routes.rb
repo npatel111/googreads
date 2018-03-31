8e8c6e90d41ac0de61fd6cd36d4b8e863ac934e7
Niti Nrw <niti.patel@flatironschool.com>
2018-03-31 00:43:37 -0400

    remove new, edit, and destroy methods for authors

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :genres, only: [:index, :show]
   resources :reviews
   resources :books
-  resources :authors
+  resources :authors, only: [:index, :show]
   resources :shelves
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
